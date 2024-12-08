89215de082122be7f222eda4815f18d71291c07c
velievrauf <145988152+velievrauf@users.noreply.github.com>
2024-12-08 23:58:43 +0300

    Add files via upload

---
+++ lab_6/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Рауф/OOP_6
+# Build directory: C:/Users/Рауф/OOP_6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("include")
+subdirs("src")
+subdirs("test")

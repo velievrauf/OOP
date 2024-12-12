6af41eb6aaa5e19a663927988d282347b68dc04f
velievrauf <145988152+velievrauf@users.noreply.github.com>
2024-12-12 15:21:04 +0300

    Delete lab_6 directory

--- lab_6/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Рауф/OOP_6
-# Build directory: C:/Users/Рауф/OOP_6/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("include")
-subdirs("src")
-subdirs("test")
